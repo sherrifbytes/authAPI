2e017634d3f6d646710b7bf2b745ee658259632d
sherrifbytes <chidyess@gmail.com>
2022-02-06 09:24:29 +0200

    updated

--- src/routes/auth/user.routes.ts
+++ src/routes/auth/user.routes.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import { createUserHandler } from '../../controller/user.controller';
 import validateResource from '../../Middleware/validateResource';
 import { createUserSchema } from '../../schema/user.schema';
 
 const router = express.Router();
 
 // routes
-router.post("/api/users", validateResource(createUserSchema), (req, res) => {
-    res.sendStatus(200)
-});
+router.post(
+    "/api/users", 
+    validateResource(createUserSchema), 
+    createUserHandler
+);
 
 export default router;
